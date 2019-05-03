6d4da8bb3871d3ee5b2ce2549f76be072dd2fcbb
Michael VanOverbeek <31224503+alkalinethunder@users.noreply.github.com>
2019-05-03 17:07:01 -0400

    Support for uploading and downloading files on hacked systems.

--- Source/ProjectOglowia/Private/ReverseShell.cpp
+++ Source/ProjectOglowia/Private/ReverseShell.cpp
@@ -31,6 +31,9 @@
 
 #include "ReverseShell.h"
 #include "ReverseShellCommandHandler.h"
+#include "UserContext.h"
+#include "PeacegateFileSystem.h"
+#include "FileUtilities.h"
 
 bool AReverseShell::RunSpecialCommand(UConsoleContext* InConsole, FString InCommand, TArray<FString> Arguments) 
 {
@@ -39,12 +42,145 @@ bool AReverseShell::RunSpecialCommand(UConsoleContext* InConsole, FString InComm
 
     if(InCommand == "upload")
     {
+        // First argument is source. Second argument is destination.
+        if(Arguments.Num() < 3)
+        {
+            InConsole->WriteLine("Usage: upload <source> <destination>");
+            this->FinishSpecialCommand();
+            return true;
+        }
 
+        // Get the source and destination from the argument list.  Docopt is no bueno for these commands.
+        FString Source = Arguments[1];
+        FString Destination = Arguments[2];
+
+        // Get the filesystem of the source computer.
+        UPeacegateFileSystem* SourceFileSystem = this->GetUserContext()->GetHacker()->GetFilesystem();
+
+        // And for the destination computer.
+        UPeacegateFileSystem* DestinationFileSystem = this->GetUserContext()->GetFilesystem();
+
+        // Check if the source file actually exists.
+        if(!SourceFileSystem->FileExists(Source))
+        {
+            InConsole->WriteLine("Error: File not found: " + Source);
+            this->FinishSpecialCommand();
+            return true;
+        }
+
+        // If the destination path is a directory, then the destination becomes <destination>/<sourceFileName>.
+        if(DestinationFileSystem->DirectoryExists(Destination))
+        {
+            FString SourceName = UFileUtilities::GetNameFromPath(Source);
+
+            if(Destination.EndsWith("/"))
+                Destination = Destination + SourceName;
+            else
+                Destination = Destination + "/" + SourceName;
+        }
+
+        // Read the file record info at the source.
+        FFileRecord SourceRecord = SourceFileSystem->GetFileRecord(Source);
+
+        // If it's a text file then we'll use the ReadText and WriteText methods.
+        if(SourceRecord.RecordType == EFileRecordType::Text)
+        {
+            EFilesystemStatusCode StatusCode;
+            FString SourceText;
+
+            // Read the text of the source.
+            if(SourceFileSystem->ReadText(Source, SourceText, StatusCode))
+            {
+                // Write the source text to the destination file.
+                DestinationFileSystem->WriteText(Destination, SourceText);
+            }
+            else
+            {
+                InConsole->WriteLine("Error occured while reading source file text.");
+                this->FinishSpecialCommand();
+                return true;
+            }
+        }
+        else
+        {
+            // Write the low-level record directly to the destination filesystem.
+            DestinationFileSystem->SetFileRecord(Destination, SourceRecord.RecordType, SourceRecord.ContentID);
+        }
+
+        InConsole->WriteLine("Uploaded " + Source + " to " + Destination + " successfully.");
+        this->FinishSpecialCommand();
         return true;
     }
     if(InCommand == "download")
     {
 
+        // First argument is source. Second argument is destination.
+        if(Arguments.Num() < 3)
+        {
+            InConsole->WriteLine("Usage: download <source> <destination>");
+            this->FinishSpecialCommand();
+            return true;
+        }
+
+        // Get the source and destination from the argument list.  Docopt is no bueno for these commands.
+        FString Source = Arguments[1];
+        FString Destination = Arguments[2];
+
+        // Get the filesystem of the source computer.
+        UPeacegateFileSystem* SourceFileSystem = this->GetUserContext()->GetFilesystem();
+
+        // And for the destination computer.
+        UPeacegateFileSystem* DestinationFileSystem = this->GetUserContext()->GetHacker()->GetFilesystem();
+
+        // Check if the source file actually exists.
+        if(!SourceFileSystem->FileExists(Source))
+        {
+            InConsole->WriteLine("Error: File not found: " + Source);
+            this->FinishSpecialCommand();
+            return true;
+        }
+
+        // If the destination path is a directory, then the destination becomes <destination>/<sourceFileName>.
+        if(DestinationFileSystem->DirectoryExists(Destination))
+        {
+            FString SourceName = UFileUtilities::GetNameFromPath(Source);
+
+            if(Destination.EndsWith("/"))
+                Destination = Destination + SourceName;
+            else
+                Destination = Destination + "/" + SourceName;
+        }
+
+        // Read the file record info at the source.
+        FFileRecord SourceRecord = SourceFileSystem->GetFileRecord(Source);
+
+        // If it's a text file then we'll use the ReadText and WriteText methods.
+        if(SourceRecord.RecordType == EFileRecordType::Text)
+        {
+            EFilesystemStatusCode StatusCode;
+            FString SourceText;
+
+            // Read the text of the source.
+            if(SourceFileSystem->ReadText(Source, SourceText, StatusCode))
+            {
+                // Write the source text to the destination file.
+                DestinationFileSystem->WriteText(Destination, SourceText);
+            }
+            else
+            {
+                InConsole->WriteLine("Error occured while reading source file text.");
+                this->FinishSpecialCommand();
+                return true;
+            }
+        }
+        else
+        {
+            // Write the low-level record directly to the destination filesystem.
+            DestinationFileSystem->SetFileRecord(Destination, SourceRecord.RecordType, SourceRecord.ContentID);
+        }
+
+        InConsole->WriteLine("Uploaded " + Source + " to " + Destination + " successfully.");
+        this->FinishSpecialCommand();
         return true;
     }
     
