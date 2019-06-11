9bc193b89dc7a80c93120db241687b484f003699
Michael VanOverbeek <31224503+alkalinethunder@users.noreply.github.com>
2019-06-11 08:21:23 -0400

    Perform two blur passes with the BackgroundBlur.

--- ThePeacenet.Gui/Controls/BackgroundBlur.cs
+++ ThePeacenet.Gui/Controls/BackgroundBlur.cs
@@ -16,14 +16,8 @@ namespace ThePeacenet.Gui.Controls
 
         public override void Draw(IGuiContext context, IGuiRenderer renderer, float deltaSeconds)
         {
-            renderer.FillRectangle(BoundingRectangle, Color.Black * 0.25f);
-
             // Grab the on-screen portion of the control's bounding box
             var visibleRect = Rectangle.Intersect(BoundingRectangle, renderer.BoundingRectangle);
-
-            // Use the blur effect.
-            renderer.SetRenderEffect(RenderEffect.Blur);
-            renderer.SetGaussianParameters(1.0f / visibleRect.Width, 1.0f / visibleRect.Height, BlurAmount);
 
             // If there's nothing visible, STOP RIGHT NOW or the game'll crash.
             if (visibleRect.Width * visibleRect.Height == 0) return;
@@ -45,9 +39,35 @@ namespace ThePeacenet.Gui.Controls
             // Put the data in our texture so we can draw it as a sprite.
             _blurTexture.SetData<Color>(_pixelData);
 
+            // Use the blur effect.
+            renderer.SetRenderEffect(RenderEffect.Blur);
+            
+            // This computes the sample weights and sample offsets for the first Gaussian pass.
+            // The first pass is a horizontal blur which we render to the back buffer.
+            renderer.SetGaussianParameters(1.0f / visibleRect.Width, 0, BlurAmount);
+
             // And now we draw it!  TODO: Use a blur effect.
             renderer.DrawBrush(visibleRect, new Brush(Color.White, _blurTexture, new Thickness(0), Size2.Empty, BrushType.Image));
 
+            // Now we need to end the batch operation.  This causes the above sprite to be blitted into the back buffer.  We need
+            // to do this so we can grab the now-blurred back buffer pixel data.
+            renderer.End();
+
+            // Grab the blurred back buffer data.
+            context.GetBackBufferData(visibleRect, _pixelData);
+
+            // Blit it into our blur texture so we can apply a second blur pass on it.
+            _blurTexture.SetData<Color>(_pixelData);
+
+            // Begin another draw call with the blur effect still enabled.
+            // We're also going to compute the sample weights and sample offsets for a vertical
+            // Gaussian blur.
+            renderer.Begin();
+            renderer.SetGaussianParameters(0, 1.0f / visibleRect.Height, BlurAmount);
+            
+            // And now we render the texture to the screen! Again.  Now it's vertically blurred.
+            renderer.DrawBrush(visibleRect, new Brush(Color.White, _blurTexture, new Thickness(0), Size2.Empty, BrushType.Image));
+            
             // And stop using the blur effect.
             renderer.SetRenderEffect(RenderEffect.None);
 
