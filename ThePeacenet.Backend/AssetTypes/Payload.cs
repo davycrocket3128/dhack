93a822eca7b77f5714cf0704312c1981b5f24409
Michael VanOverbeek <31224503+alkalinethunder@users.noreply.github.com>
2019-06-15 12:00:05 -0400

    Port over base class for Payloads.

---
+++ ThePeacenet.Backend/AssetTypes/Payload.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThePeacenet.Backend.OS;
+
+namespace ThePeacenet.Backend.AssetTypes
+{
+    public abstract class Payload
+    {
+        protected abstract void OnPayloadDeployed(IConsoleContext Console, UserContext OriginUser, UserContext TargetUser);
+
+        protected void Disconnect()
+        {
+            Disconnected?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler Disconnected;
+
+        public void DeployPayload(IConsoleContext OriginConsole, UserContext OriginUser, UserContext TargetUser)
+        {
+            // Create a networked console context that outputs to the origin but uses the hacked user as a means of
+            // gaining a Peacegate context.
+            NetworkedConsoleContext HackerContext = new NetworkedConsoleContext(OriginConsole, TargetUser);
+            
+            // This console is passed to the deriving payload methods - the payload is thus run in the context of the hacked system.
+            this.OnPayloadDeployed(HackerContext, OriginUser, TargetUser);
+
+        }
+    }
+}
---
+++ ThePeacenet.Backend/OS/NetworkedConsoleContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePeacenet.Backend.OS
+{
+    public class NetworkedConsoleContext : IConsoleContext
+    {
+        private UserContext _hacker = null;
+        private IConsoleContext _origin = null;
+        private string _working = null;
+
+        public NetworkedConsoleContext(IConsoleContext origin, UserContext hacker)
+        {
+            _origin = origin;
+            _hacker = hacker;
+
+            _working = User.HomeFolder;
+        }
+
+        public UserContext User => _hacker;
+
+        public string WorkingDirectory
+        {
+            get => _working;
+            set {
+                if(_working != value && User.FileSystem.DirectoryExists(value))
+                {
+                    _working = value;
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            _origin.Clear();
+        }
+
+        public bool GetLine(out string text)
+        {
+            return _origin.GetLine(out text);
+        }
+
+        public void OverWrite(string text)
+        {
+            _origin.OverWrite(text);
+        }
+
+        public void OverWrite(string format, params object[] args)
+        {
+            _origin.OverWrite(format, args);
+        }
+
+        public void Write(string text)
+        {
+            _origin.Write(text);
+        }
+
+        public void Write(string format, params object[] args)
+        {
+            _origin.Write(format, args);
+        }
+
+        public void WriteLine(string text)
+        {
+            _origin.WriteLine(text);
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            _origin.WriteLine(format, args);
+        }
+    }
+}
