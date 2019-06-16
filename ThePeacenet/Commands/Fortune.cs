731454f578a0d7bcc0b817bd768c88809612e719
AShifter <loganr.lowe@gmail.com>
2019-06-16 00:09:35 -0600

    Added the Fortune command. Incomplete.
    I'm not sure how we should go about implementing this one - I didn't want to hardcode anything, and I forgot where the REST API that we used last time to grab random fortunes is.

---
+++ ThePeacenet/Commands/Fortune.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThePeacenet.Backend.AssetAttributes;
+using ThePeacenet.Backend.Shell;
+
+namespace ThePeacenet.Commands
+{
+    [Description("Prints a fortune, like you might find in a fortune cookie.")]
+    [UnlockedByDefault]
+    [Usage("none")]
+    public class Fortune : Command
+    {
+        protected override void OnRun(string[] args)
+        {
+            // Honestly I have absolutely no idea how we should go about implementing this command, so for now, this is your fortune.
+            Console.WriteLine("Those that work with haste are probably a waste.");
+        }
+    }
+}
