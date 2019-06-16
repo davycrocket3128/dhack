82f98095fbe2c4cc872648319647c183921d5b7b
Michael VanOverbeek <31224503+alkalinethunder@users.noreply.github.com>
2019-06-16 12:33:20 -0400

    Begin porting icedog's UI over.

---
+++ ThePeacenet/GuiHandlers/Icedog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThePeacenet.Backend;
+
+namespace ThePeacenet.GuiHandlers
+{
+    public class Icedog : GuiHandler
+    {
+        public void Icedog_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
